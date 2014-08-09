6dba8268cd8eab169d1a257b3a7af5781eb9f277
Andrey Sumin <an.sumin@hh.ru>
2014-08-09 12:53:41 +0400

    set line width to 0

--- sre/config.js
+++ sre/config.js
@@ -1,5 +1,5 @@
 config = {
     graphiteUrl: 'http://graphite.hh.ru' + ('test' in window.localStorage ? '/test' : ''),
-    basicGraphParams: 'fontSize=10&fontName=FreeMono&lineWidth=1&areaAlpha=0.85&yMin=0',
+    basicGraphParams: 'fontSize=10&fontName=FreeMono&lineWidth=0&areaAlpha=0.85&yMin=0',
     updateInterval: 15000,
 };
